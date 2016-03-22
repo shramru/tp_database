cddfd866bd48fb66e5d729cd5bfcd96ec44e04f5
Vladislav <shramru@gmail.com>
2016-03-23 01:46:24 +0300

    dump

---
+++ database_dump.sql
@@ -0,0 +1,208 @@
+CREATE DATABASE  IF NOT EXISTS `db_techopark` /*!40100 DEFAULT CHARACTER SET utf8 COLLATE utf8_unicode_ci */;
+USE `db_techopark`;
+-- MySQL dump 10.13  Distrib 5.7.9, for linux-glibc2.5 (x86_64)
+--
+-- Host: localhost    Database: db_techopark
+-- ------------------------------------------------------
+-- Server version	5.6.28-0ubuntu0.15.10.1
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `forum`
+--
+
+DROP TABLE IF EXISTS `forum`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `forum` (
+  `fID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `short_name` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `user` varchar(255) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`fID`),
+  UNIQUE KEY `name_UNIQUE` (`name`) USING BTREE,
+  UNIQUE KEY `short_name_UNIQUE` (`short_name`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `post`
+--
+
+DROP TABLE IF EXISTS `post`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `post` (
+  `pID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `parent` int(11) DEFAULT NULL,
+  `isApproved` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `isHighlighted` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `isEdited` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `isSpam` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `isDeleted` tinyint(1) NOT NULL DEFAULT '0',
+  `date` datetime NOT NULL,
+  `message` text CHARACTER SET utf8 NOT NULL,
+  `user` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `forum` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `likes` int(11) unsigned NOT NULL DEFAULT '0',
+  `dislikes` int(11) unsigned NOT NULL DEFAULT '0',
+  `points` int(11) NOT NULL DEFAULT '0',
+  `path` text COLLATE utf8_unicode_ci,
+  `tID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`pID`)
+) ENGINE=InnoDB AUTO_INCREMENT=33 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `thread`
+--
+
+DROP TABLE IF EXISTS `thread`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `thread` (
+  `tID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `isDeleted` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `forum` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `isClosed` tinyint(1) unsigned NOT NULL,
+  `user` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `date` datetime NOT NULL,
+  `message` mediumblob NOT NULL,
+  `slug` varchar(255) CHARACTER SET utf8 NOT NULL,
+  `dislikes` int(11) unsigned DEFAULT '0',
+  `likes` int(11) unsigned DEFAULT '0',
+  `points` int(11) DEFAULT '0',
+  `posts` int(11) unsigned DEFAULT '0',
+  `title` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  PRIMARY KEY (`tID`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user`
+--
+
+DROP TABLE IF EXISTS `user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user` (
+  `uID` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `isAnonymous` tinyint(1) unsigned NOT NULL DEFAULT '0',
+  `username` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `about` text COLLATE utf8_unicode_ci,
+  `name` varchar(255) CHARACTER SET utf8 DEFAULT NULL,
+  `email` varchar(255) CHARACTER SET utf8 NOT NULL,
+  PRIMARY KEY (`uID`),
+  UNIQUE KEY `email_UNIQUE` (`email`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_thread`
+--
+
+DROP TABLE IF EXISTS `user_thread`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_thread` (
+  `user` varchar(255) NOT NULL,
+  `tID` int(11) NOT NULL,
+  UNIQUE KEY `ut` (`user`,`tID`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Table structure for table `user_user`
+--
+
+DROP TABLE IF EXISTS `user_user`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `user_user` (
+  `follower` varchar(255) NOT NULL,
+  `followee` varchar(255) NOT NULL,
+  UNIQUE KEY `ff` (`follower`,`followee`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping routines for database 'db_techopark'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `clear` */;
+ALTER DATABASE `db_techopark` CHARACTER SET utf8 COLLATE utf8_general_ci ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `clear`()
+BEGIN
+TRUNCATE TABLE forum;
+TRUNCATE TABLE post;
+TRUNCATE TABLE thread;
+TRUNCATE TABLE user;
+TRUNCATE TABLE user_thread;
+TRUNCATE TABLE user_user;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+ALTER DATABASE `db_techopark` CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
+/*!50003 DROP PROCEDURE IF EXISTS `status` */;
+ALTER DATABASE `db_techopark` CHARACTER SET utf8 COLLATE utf8_general_ci ;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8 */ ;
+/*!50003 SET character_set_results = utf8 */ ;
+/*!50003 SET collation_connection  = utf8_general_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `status`()
+BEGIN
+DECLARE user INT;
+DECLARE thread INT;
+DECLARE forum INT;
+DECLARE post INT;
+
+SELECT COUNT(*) INTO user FROM user;
+SELECT COUNT(*) INTO thread FROM thread;
+SELECT COUNT(*) INTO forum FROM forum;
+SELECT COUNT(*) INTO post FROM post;
+
+SELECT user, thread, forum, post;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+ALTER DATABASE `db_techopark` CHARACTER SET utf8 COLLATE utf8_unicode_ci ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2016-03-23  1:45:12
